7902bed8151cfafc2bf01ddaf58aa6ea43a16b65
Mohamed247 <mohamedmotaz247@yahoo.com>
2020-12-20 20:25:58 +0200

    Add scatterplot

--- Formulas/formulas.py
+++ Formulas/formulas.py
@@ -1,12 +1,23 @@
 #not a graph
 
 #functions to calculate standard deviation
+def calcMean(groupedData = False):
+    return 0
 
+def calc():
+    return 0
+
+def calcStandardDeviation():
+    return 0
 #variance
 
+def calcVariance():
+    return 0
 #z score
-
+def calcZScore():
+    return 0
 #b1 b0
+
 
 
 #calculate correlation and regression
