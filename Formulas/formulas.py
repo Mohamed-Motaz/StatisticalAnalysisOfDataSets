fb7085d993e90be1a2de1f6431aa53bb115cf489
Mohamed247 <mohamedmotaz247@yahoo.com>
2020-12-20 22:22:42 +0200

    formulas

--- Formulas/formulas.py
+++ Formulas/formulas.py
@@ -1,24 +1,67 @@
-#not a graph
+import math
 
-#functions to calculate standard deviation
-def calcMean(groupedData = False):
-    return 0
+dummyData = [1,2,3,4,5,6,7,8,9,10]
+dummyDataFrequency = [1,2,3,4,5,6,7,8,9,10]
 
-def calc():
-    return 0
+def calcMeanUnGroupedData(dataSet):
+    """
+    Calculate mean for grouped data using the value
+    """
+    mean = 0
+    for element in dataSet:
+        mean += element
+    mean = mean / len(dataSet)
+    return float(mean)
 
-def calcStandardDeviation():
-    return 0
-#variance
+print(calcMeanUnGroupedData(dummyData))    
 
-def calcVariance():
-    return 0
-#z score
-def calcZScore():
-    return 0
+def calcMeanGroupedData(dataSet, frequencySet):
+    """
+    Calculate mean for grouped data using the value
+    """
+    if len(dataSet) != len(frequencySet):
+        raise Exception("Data set and frequency set sizes not equal")
+        
+    mean = 0
+    numberOfElements = 0    
+    for i in range(0, len(dataSet)):
+        mean += dataSet[i] * frequencySet[i]
+        numberOfElements += frequencySet[i]
+
+    mean = mean / numberOfElements
+    return float(mean)
+
+print(calcMeanGroupedData(dummyData, dummyDataFrequency))
+
+
+def calcVarianceGrouped(dataSet, frequencySet):
+    mean = calcMeanGroupedData(dataSet, frequencySet)
+    sum = 0
+
+    for i in range(0, len(dataSet)):
+        sum += pow(dataSet[i] - mean, 2) * frequencySet[i]
+    
+    return float(sum / (len(dataSet) - 1))
+
+def calcStandardDeviationGrouped(dataSet, frequencySet):
+    return sqrt(calcVarianceGrouped(dataSet, frequencySet))
+
+def calcVarianceUnGrouped(dataSet):
+    mean = calcMeanUnGroupedData(dataSet)
+    sum = 0
+
+    for element in dataSet:
+        sum += pow(element - mean, 2)
+
+    return float(sum / (len(dataSet) - 1))
+
+
+def calcStandardDeviationUnGrouped(dataSet):
+    return sqrt(calcVarianceUnGrouped(dataSet))
+
+def calcZScoreUnGrouped(idx, dataSet):
+    return (dataSet[idx] - calcMeanUnGroupedData(dataSet) / calcStandardDeviationUnGrouped(dataSet))
+
+
 #b1 b0
-
-
-
-#calculate correlation and regression
 
--- Graphs/piechart.py
+++ Graphs/piechart.py
@@ -1,1 +1,9 @@
-#salma
+import matplotlib.pyplot as plt
+
+Tasks = [300,500,700]
+
+my_labels = 'Tasks Pending','Tasks Ongoing','Tasks Completed'
+plt.pie(Tasks, labels=my_labels, autopct='%1.1f%%')
+plt.title('My Tasks')
+plt.axis('equal')
+plt.show()
