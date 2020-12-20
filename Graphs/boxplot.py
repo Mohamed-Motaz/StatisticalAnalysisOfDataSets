0505b8a044bfaa84c4689adbd920eaebf2d57b2b
Mohamed247 <mohamedmotaz247@yahoo.com>
2020-12-21 00:42:27 +0200

    final

--- Graphs/boxplot.py
+++ Graphs/boxplot.py
@@ -1,2 +1,22 @@
-#bedo
+import matplotlib.pyplot as plt 
+import numpy as np 
+    
+# Creating dataset 
+np.random.seed(10)
 
+data =  [0,5.6,8.7,14.1,14.1,15,17.2,19.2,19.3,24.1,27.7]
+  
+Bedoazim,ax1=plt.subplots()
+
+red_square = dict(markerfacecolor='r', marker='s')
+
+ax1.boxplot(data, flierprops=red_square,meanline=True,showfliers=True)
+
+ax1.set_title('BoxPlot')
+#patch.set_facecolor("pink")
+  
+# Creating plot 
+ax1.boxplot(data) 
+  
+# show plot 
+plt.show()
--- LinkFormsToPython/LinkFormsToPython/Formulas.cs
+++ LinkFormsToPython/LinkFormsToPython/Formulas.cs
@@ -1,12 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace LinkFormsToPython
+{
+    class Formulas
+    {
+        dummyData = [1,2,3,4,5,6,7,8,9,10]
+        dummyData2 = [1,2,3,4,5,6,7,8,9,10]
+        dummyDataFrequency = [1,2,3,4,5,6,7,8,9,10]
 
-import math
-
-dummyData = [1,2,3,4,5,6,7,8,9,10]
-dummyData2 = [1,2,3,4,5,6,7,8,9,10]
-dummyDataFrequency = [1,2,3,4,5,6,7,8,9,10]
-
-def calcMeanUnGroupedData(dataSet):
+        def calcMeanUnGroupedData(dataSet):
     """
     Calculate mean for grouped data
     """
@@ -14,27 +20,28 @@ def calcMeanUnGroupedData(dataSet):
     for element in dataSet:
         mean += element
     mean = mean / len(dataSet)
-    return float(mean)
+    return float (mean)
 
-print(calcMeanUnGroupedData(dummyData))    
+ print(calcMeanUnGroupedData(dummyData))
 
 def calcMeanGroupedData(dataSet, frequencySet):
     """
     Calculate mean for grouped data
     """
-    if len(dataSet) != len(frequencySet):
+    if len(dataSet) != len(frequencySet) :
         raise Exception("Data set and frequency set sizes not equal")
-        
+
+
     mean = 0
     numberOfElements = 0    
-    for i in range(0, len(dataSet)):
+    for i in range(0, len(dataSet)) :
         mean += dataSet[i] * frequencySet[i]
         numberOfElements += frequencySet[i]
 
     mean = mean / numberOfElements
-    return float(mean)
+    return float (mean)
 
-print(calcMeanGroupedData(dummyData, dummyDataFrequency))
+ print(calcMeanGroupedData(dummyData, dummyDataFrequency))
 
 def calcVarianceGrouped(dataSet, frequencySet):
     """
@@ -43,10 +50,10 @@ def calcVarianceGrouped(dataSet, frequencySet):
     mean = calcMeanGroupedData(dataSet, frequencySet)
     sum = 0
 
-    for i in range(0, len(dataSet)):
+    for i in range(0, len(dataSet)) :
         sum += pow(dataSet[i] - mean, 2) * frequencySet[i]
     
-    return float(sum / (len(dataSet) - 1))
+    return float (sum / (len(dataSet) - 1))
 
 def calcStandardDeviationGrouped(dataSet, frequencySet):
     """
@@ -64,7 +71,7 @@ def calcVarianceUnGrouped(dataSet):
     for element in dataSet:
         sum += pow(element - mean, 2)
 
-    return float(sum / (len(dataSet) - 1))
+    return float (sum / (len(dataSet) - 1))
 
 
 def calcStandardDeviationUnGrouped(dataSet):
@@ -85,16 +92,16 @@ def calcPearsonR(dataSet1, dataSet2):
     """
     sum = 0
 
-    for i in range(0, len(dataSet1)):
+    for i in range(0, len(dataSet1)) :
         sum += calcZScoreUnGrouped(i, dataSet1) * calcZScoreUnGrouped(i, dataSet2)
 
-    return float(sum / (len(dataSet1) - 1))
+    return float (sum / (len(dataSet1) - 1))
 
 def calcCoefficientOfDetermination(dataSet1, dataSet2):
     """
     Calculate coefficient of determination for two datasets
     """
-    return float(pow(calcPearsonR(dataSet1, dataSet2), 2))
+    return float (pow(calcPearsonR(dataSet1, dataSet2), 2))
 
 def calcBOne(dataSet1, dataSet2):
     """
@@ -104,7 +111,7 @@ def calcBOne(dataSet1, dataSet2):
     sX = calcStandardDeviationUnGrouped(dataSet1)
     sY = calcStandardDeviationUnGrouped(dataSet2)
 
-    return float( (sY / sX)  * r )
+    return float ((sY / sX)  * r )
 
 
 def calcBNode(dataSet1, dataSet2):
@@ -115,7 +122,7 @@ def calcBNode(dataSet1, dataSet2):
     xMean = calcMeanUnGroupedData(dataSet1)
     bOne = calcBOne(dataSet1, dataSet2)
 
-    return float(yMean - bOne * xMean) 
+    return float (yMean - bOne* xMean) 
 
 
 def calcPredictedY(dataSet1, dataSet2, x):
@@ -125,9 +132,9 @@ def calcPredictedY(dataSet1, dataSet2, x):
     bOne = calcBOne(dataSet1, dataSet2)
     bNode = calcBNode(dataSet1, dataSet2)
 
-    return float(bOne * x + bNode)
+    return float (bOne* x + bNode)
 
 def calcNumberOfClassesForHistogram(dataSet):
-    return int(math.ceil(math.sqrt(len(dataSet))))
-
-
+    return int (math.ceil(math.sqrt(len(dataSet))))
+    }
+}
