70f5881623862bee4420cfabc968a44132f39b83
Mohamed247 <mohamedmotaz247@yahoo.com>
2020-12-20 14:07:19 +0200

    Files To Work

---
+++ bargraph.py
@@ -0,0 +1,10 @@
+#agina
+#create bargraph
+
+list1 = [1,2,3,4,5]
+list1names = ["1", "2", "3", "4", "5"]
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
---
+++ formulas.py
@@ -0,0 +1,13 @@
+#not a graph
+
+#functions to calculate standard deviation
+
+#variance
+
+#z score
+
+#b1 b0
+
+
+#calculate correlation and regression
+
